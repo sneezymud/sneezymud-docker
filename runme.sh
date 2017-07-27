9c35c1dcb568f03109ed8b5e029e152908094a5a
Elmo Todurov <elmo.todurov@eesti.ee>
2017-07-27 16:11:18 +0300

    GDB compatibility

--- runme.sh
+++ runme.sh
@@ -2,4 +2,5 @@
 
 cd `dirname $0`
 docker build -t sneezy docker
-docker run --name sneezy -it -p 7900:7900 -v `pwd`/sneezymud:/home/sneezy/sneezymud -v `pwd`/mysql:/var/lib/mysql sneezy
+docker rm sneezy  # nuke the previous run if needed
+docker run --name sneezy --cap-add=SYS_PTRACE -it -p 7900:7900 -v `pwd`/sneezymud:/home/sneezy/sneezymud -v `pwd`/mysql:/var/lib/mysql sneezy
