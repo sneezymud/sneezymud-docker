e02eb8aed089447574210b95f017dc66bb240f2f
Elmo Todurov <elmo.todurov@eesti.ee>
2017-10-15 22:56:31 +0300

    Added --rm to run. Hmm, what about core files?

--- runme.sh
+++ runme.sh
@@ -2,5 +2,4 @@
 
 cd `dirname $0`
 docker build -t sneezy docker
-docker rm sneezy  # nuke the previous run if needed
-docker run --name sneezy --cap-add=SYS_PTRACE -it -p 7900:7900 -v `pwd`/sneezymud:/home/sneezy/sneezymud -v `pwd`/mysql:/var/lib/mysql sneezy
+docker run --rm --name sneezy --cap-add=SYS_PTRACE -it -p 7900:7900 -v `pwd`/sneezymud:/home/sneezy/sneezymud -v `pwd`/mysql:/var/lib/mysql sneezy
