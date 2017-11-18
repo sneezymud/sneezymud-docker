666405fed5d6e1180629f8ef7a86f37ebf2c5d6e
Elmo Todurov <elmo.todurov@eesti.ee>
2017-11-18 07:40:23 +0000

    Fix scons git version with submodule. Add helpful error if no source

--- recompile.sh
+++ recompile.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
 cd `dirname $0`
+
+if [ ! -d sneezymud/code ]; then
+    echo "Please run git submodule update --init"
+    exit 1
+fi
+
+echo 'gitdir: ../.git/modules/sneezymud' > sneezymud/.git
 docker build -t sneezy docker
 docker run -it --rm -v `pwd`:/home/sneezy/sneezymud-docker sneezy
