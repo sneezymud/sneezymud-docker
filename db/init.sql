0e4f9caecce25e1b982ad1076783a9c47334e944
Josh Knapp <joshknapp@gmail.com>
2018-11-27 15:02:19 -0800

    additions to web

--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,11 @@ CREATE USER 'sneezy'@'sneezy.sneezymuddocker_default' IDENTIFIED BY 'password';
 CREATE USER 'sneezy'@'localhost' IDENTIFIED BY 'password';
 
 CREATE DATABASE IF NOT EXISTS immortal CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON immortal.* to 'sneezy'@'sneezy.sneezymuddocker_default';
-GRANT ALL ON immortal.* to 'sneezy'@'localhost';
+--GRANT ALL ON immortal.* to 'sneezy'@'sneezy.sneezymuddocker_default';
+--GRANT ALL ON immortal.* to 'sneezy'@'localhost';
+GRANT ALL ON immortal.* to 'sneezy'@'%';
 
 CREATE DATABASE IF NOT EXISTS sneezy CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON sneezy.* to 'sneezy'@'sneezy.sneezymuddocker_default';
-GRANT ALL ON sneezy.* to 'sneezy'@'localhost';
+--GRANT ALL ON sneezy.* to 'sneezy'@'sneezy.sneezymuddocker_default';
+--GRANT ALL ON sneezy.* to 'sneezy'@'localhost';
+GRANT ALL ON sneezy.* to 'sneezy'@'%';
--- web/app.py
+++ web/app.py
@@ -1,11 +1,12 @@
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "SneezyCRUD / construction in progress"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
