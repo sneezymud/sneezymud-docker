981fccf0c2a832d2e0455052a76df80bfc13c14a
Josh Knapp <joshknapp@gmail.com>
2018-11-29 14:43:09 -0800

    //web talks to sneezy database now !!!!11

--- web/app.py
+++ web/app.py
@@ -4,9 +4,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+import mysql.connector as mariadb
+
+mariadb_connection = mariadb.connect(host='db', user='sneezy', password='password', database='sneezy')
+cursor = mariadb_connection.cursor()
+
+for zone_name in cursor:
+    print("Zone: {}").format(zone_name)
+
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    cursor.execute("SELECT zone_name FROM zone")
+    zones = list(cursor)
+    return render_template("home.html", zones=zones)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
