a58f4fc8ba7c542d6e391b763da9748ed7b31825
Josh Knapp <joshknapp@gmail.com>
2018-12-07 14:16:15 -0800

    working on user auth

--- web/app.py
+++ web/app.py
@@ -2,8 +2,13 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
+import crypt
+
 
 app = Flask(__name__)
+
+
 
 #import mysql.connector as mariadb
 #mariadb_connection = mariadb.connect(host='db', user='sneezy', password='password', database='sneezy')
@@ -15,17 +20,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{usr}:{passwd}@{host}/{
 
 db = SQLAlchemy(app)
 
- #`zone_nr` int(11) NOT NULL,
-  #`zone_name` varchar(255) NOT NULL default '',
-  #`zone_enabled` int(11) default NULL,
-  #`bottom` int(11) default NULL,
-  #`top` int(11) default NULL,
-  #`reset_mode` int(11) default NULL,
-  #`lifespan` int(11) default NULL,
-  #`age` int(11) default NULL,
-  #`util_flag` int(11) default NULL,
-  #PRIMARY KEY  (`zone_nr`)
+#login = LoginManager(app)
+#class User(UserMixin, db.Model):
 
+
+class Account(db.Model):
+    account_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    email = db.Column(db.String(80))
+    name = db.Column(db.String(80))
+    passwd = db.Column(db.String(13))
+    birth = db.Column(db.Integer)
+    term = db.Column(db.Integer)
+    time_adjust = db.Column(db.Integer)
+    flags = db.Column(db.Integer)
+    last_logon = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "<Name: {}>".format(self.name)
 
 class Zone(db.Model):
     zone_nr = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
@@ -43,10 +54,9 @@ class Zone(db.Model):
 
 @app.route("/")
 def hello():
-    #cursor.execute("SELECT zone_name FROM zone")
-    #zones = list(cursor)
     zones = Zone.query.all()
-    return render_template("home.html", zones=zones)
+    accounts = Account.query.all()
+    return render_template("home.html", zones= zones, accounts=accounts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
