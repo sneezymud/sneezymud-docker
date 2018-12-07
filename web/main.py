b76c0f66e73c07aab82aa13a5c1965df9a73fd00
Elmo Todurov <elmo.todurov@eesti.ee>
2018-12-08 01:07:29 +0200

    Added auth against Sneezy usernames

---
+++ web/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{usr}:{passwd}@{host}/{db}'.format(
+    usr='sneezy', passwd='password', host='db', db='sneezy')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from controller import *
+print("Routes:")
+print(app.url_map)
+
+if __name__ == "__main__":
+    print("Starting")
+    app.run(host="0.0.0.0", debug=True)
