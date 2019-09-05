4ffcd44d8b9ac1a61dd748273c8f5fe18d29b9ec
Josh Knapp <joshknapp@gmail.com>
2019-09-05 12:39:26 -0700

    Revert "switched to immortal db"
    This reverts commit 2ae87782acc2a7a3b835960a4608c0ccadc0364e.

--- web/main.py
+++ web/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{usr}:{passwd}@{host}/{db}'.format(
-    usr='sneezy', passwd='password', host='db', db='immortal')
+    usr='sneezy', passwd='password', host='db', db='sneezy')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
