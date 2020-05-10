2f8117f39a769a257bae312275d3cb9eff7f3eb2
Elmo Todurov <elmo.todurov@eesti.ee>
2020-05-10 23:53:41 +0300

    update recompile.sh to be cheaper on dry runs

--- recompile.sh
+++ recompile.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-docker exec sneezy bash -c 'scons -j`grep -c ^processor /proc/cpuinfo` -Q debug=1 -Q sanitize=0 -Q olevel=1'
+set -e
+
+pushd ~/sneezymud-docker/sneezymud
+
+git fetch
+if [ `git rev-parse HEAD` != `git rev-parse FETCH_HEAD` ]; then
+  pushd ~/sneezymud-docker
+  docker exec sneezy bash -c 'scons -j`grep -c ^processor /proc/cpuinfo` -Q debug=1 -Q sanitize=0 -Q olevel=1'
+  popd
+fi
+popd
