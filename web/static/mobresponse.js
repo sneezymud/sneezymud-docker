8a5f57e821f1e2947b87de0522d50d00f97921d3
Elmo Todurov <elmo.todurov@eesti.ee>
2020-05-12 00:18:49 +0300

    multi-variant editor of mobresponses

--- web/static/mobresponse.js
+++ web/static/mobresponse.js
@@ -6,30 +6,29 @@ var MobResponses = function () {
   var addBtn = document.getElementById('addNewResponse');
   var editBtn = document.getElementById('editResponse');
 
+  function enableEditing() {
+    actionElem.removeAttribute('disabled');
+    editBtn.removeAttribute('disabled');
+    addBtn.removeAttribute('disabled');
+  }
+
+  function disableEditing() {
+    actionElem.setAttribute('disabled', true);
+    editBtn.setAttribute('disabled', true);
+    addBtn.setAttribute('disabled', true);
+  }
+
   function onEditMobresponseSelected() {
-    function enable() {
-      actionElem.removeAttribute('disabled');
-      editBtn.removeAttribute('disabled');
-      addBtn.removeAttribute('disabled');
-    }
-
-    function disable() {
-      actionElem.setAttribute('disabled', true);
-      editBtn.setAttribute('disabled', true);
-      addBtn.setAttribute('disabled', true);
-    }
-
     var inputValue = inputElem.value;
-    console.debug(inputValue);
 
     if (inputValue === '') {
-      disable();
+      disableEditing();
       actionElem.value = '';
       return;
     }
 
     if (!/^\w+ "[^"]*"/.test(inputValue)) {
-      disable();
+      disableEditing();
       actionElem.value = 'Please provide a trigger and parameter, like this:\nsay "hi"';
       return;
     }
@@ -39,20 +38,50 @@ var MobResponses = function () {
 
     // existing trigger?
     if (mobResponses[trigger] !== undefined && mobResponses[trigger][param] !== undefined) {
-      var theseResponses = mobResponses[trigger][param]
+      var theseResponses = mobResponses[trigger][param];
       console.debug(theseResponses);
-      if (theseResponses.length == 1) {
-        actionElem.value = theseResponses[0].join(';\n');
-        enable()
-      } else {
-        disable();
-        actionElem.value = "There are multiple responses for this trigger. Please edit the mobresponse file directly below.";
-      }
+      populateResponsesDropdow(theseResponses);
     } else {
       // new trigger
-      enable();
+      enableEditing();
       actionElem.value = '';
     }
+  }
+
+  function clearDropdown(dd) {
+    // cloneNode is non-recursive by default, so we just drop children in one go.
+    dd.parentElement.replaceChild(dd.cloneNode(), dd);
+    console.log("cleared DD");
+  }
+
+  function populateResponsesDropdow(responses) {
+    clearDropdown(document.getElementById('responseVariant'));
+    var dropdown = document.getElementById('responseVariant');
+
+    var itemsInDropdown = 0;
+    function addToDropdown(variant) {
+      var option = document.createElement("option");
+      ++itemsInDropdown;
+      option.text = itemsInDropdown.toString();
+      option.value = itemsInDropdown.toString();
+      dropdown.appendChild(option);
+      console.log("Appended " + option.value);
+    }
+
+    if (responses.length == 1) {
+      actionElem.value = responses[0].join(';\n') + ';';
+    } else {
+      responses.forEach(addToDropdown);
+    }
+
+    dropdown.onchange = function (event) {
+      var idx = event.target.value - 1;
+      var text = responses[idx];
+      actionElem.value = text.join(';\n') + ';';
+    }
+    var event = new Event('change');
+    dropdown.dispatchEvent(event);
+    enableEditing();
   }
 
   var inputElem = document.getElementById('trigger');
