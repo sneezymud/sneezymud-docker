cb61703fe99d94d4c6ab23a331ddd8895038516a
Elmo Todurov <elmo.todurov@eesti.ee>
2020-09-04 12:38:12 +0300

    remove building DB, it is stock now

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,9 +2,7 @@
 set -e
 
 docker build -t sneezymud/sneezymud:latest docker
-docker build -t sneezymud/sneezymud-db:latest db
 docker build -t sneezymud/sneezymud-buildertools:latest web
 
 docker push sneezymud/sneezymud:latest
-docker push sneezymud/sneezymud-db:latest
 docker push sneezymud/sneezymud-buildertools:latest
