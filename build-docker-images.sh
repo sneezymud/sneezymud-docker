9d74d1105ebc468afb74ff065592aee08e668e87
Elmo Todurov <elmo.todurov@eesti.ee>
2020-11-15 00:08:01 +0200

    add building webclient

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -4,6 +4,8 @@ set -e
 TAG="${1:-latest}"
 docker build -t sneezymud/sneezymud:"$TAG" docker
 docker build -t sneezymud/sneezymud-buildertools:"$TAG" web
+docker build -t sneezymud/webclient:"$TAG" webclient
 
 docker push sneezymud/sneezymud:"$TAG"
 docker push sneezymud/sneezymud-buildertools:"$TAG"
+docker push sneezymud/webclient:"$TAG"
