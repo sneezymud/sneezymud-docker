d83fcd17e42c9d65e4ac545b4c0fe3dff00abff9
Elmo Todurov <elmo.todurov@eesti.ee>
2020-12-22 00:48:45 +0200

    update backup script to statefulsets and default namespace

--- aks-remote-backup.sh
+++ aks-remote-backup.sh
@@ -5,17 +5,15 @@ set -e
 TEMPLOCATION="/tmp"
 SNEEZYLIB="/home/sneezy"
 BACKUPDIR="."
-POD=`kubectl get pods| grep sneezymud | cut -d\  -f1`
+POD=pod/sneezymud-0
+NAMESPACE="-ndefault"
 
 # Setting the backup filename
 FNAME="$BACKUPDIR/sneezy-backup-`date +%s`.tar"
 
-# Dump the database (this takes a bit of time)
-# mysqldump -u root -p111111 --databases sneezy immortal > "$TEMPLOCATION/dbdump.sql"
-
 # Perform the backup
-kubectl exec -t pod/sneezy-db-0 -- mysqldump -h sneezy-db -u root -p111111 --databases sneezy immortal > "$TEMPLOCATION/dbdump.sql"
-(kubectl exec -t "$POD" -- tar -c --exclude='core' -C "$SNEEZYLIB" lib || true ) > "$FNAME"
+kubectl "$NAMESPACE" exec -t pod/sneezy-db-0 -- mysqldump -h sneezy-db -u root -p111111 --databases sneezy immortal > "$TEMPLOCATION/dbdump.sql"
+(kubectl "$NAMESPACE" exec -t "$POD" -- tar -c --exclude='core' -C "$SNEEZYLIB" lib || true ) > "$FNAME"
 tar --owner=sneezy:1000 --group=sneezy:1000 -rf "$FNAME" -C "$TEMPLOCATION" dbdump.sql
 xz "$FNAME"
 
