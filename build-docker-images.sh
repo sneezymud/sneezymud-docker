78780bd324cdc63072be876389c8bcb994a9b086
Elmo Todurov <elmo.todurov@eesti.ee>
2021-01-03 21:19:39 +0200

    buildscript: support branches

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,9 +2,11 @@
 set -e
 
 TAG="${1:-latest}"
-docker build -t sneezymud/sneezymud:"$TAG" docker
-docker build -t sneezymud/sneezymud-buildertools:"$TAG" web
-docker build -t sneezymud/webclient:"$TAG" webclient
+BRANCH="${2:-master}"
+
+docker build --build-arg=BRANCH="$BRANCH" -t sneezymud/sneezymud:"$TAG" docker
+docker build --build-arg=BRANCH="$BRANCH" -t sneezymud/sneezymud-buildertools:"$TAG" web
+docker build --build-arg=BRANCH="$BRANCH" -t sneezymud/webclient:"$TAG" webclient
 
 docker push sneezymud/sneezymud:"$TAG"
 docker push sneezymud/sneezymud-buildertools:"$TAG"
