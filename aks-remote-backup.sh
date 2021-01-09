0a9f3ad4234a04b6de1d29c57a51add53af1a59e
Elmo Todurov <elmo.todurov@eesti.ee>
2021-01-09 23:00:17 +0200

    add pushing the backup to Sneezy webserver, for easy pulling

--- aks-remote-backup.sh
+++ aks-remote-backup.sh
@@ -15,11 +15,13 @@ FNAME="$BACKUPDIR/sneezy-backup-`date +%s`.tar"
 kubectl "$NAMESPACE" exec -t pod/sneezy-db-0 -- mysqldump -h sneezy-db -u root -p111111 --databases sneezy immortal > "$TEMPLOCATION/dbdump.sql"
 (kubectl "$NAMESPACE" exec -t "$POD" -c sneezymud -- tar -c --exclude='core' -C "$SNEEZYLIB" lib || true ) > "$FNAME"
 tar --owner=sneezy:1000 --group=sneezy:1000 -rf "$FNAME" -C "$TEMPLOCATION" dbdump.sql
-xz "$FNAME"
 
 # Remove our temps
 rm "$TEMPLOCATION/dbdump.sql"
 
+# xz "$FNAME"
+gzip "$FNAME"
+
 # Push the backup to any online repositories.
 #drive push -no-prompt -quiet -destination backups "$FNAME"
-
+kubectl cp -c sneezy-webclient "$FNAME".gz sneezymud-0:/usr/share/nginx/html/static/sneezy-backup.tar.gz
