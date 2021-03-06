9c07dc5db0209fbe895e0fbd7973dc94be9f4dee
Elmo Todurov <elmo.todurov@eesti.ee>
2021-03-06 22:18:51 +0200

    remove obsolete files

--- mysql_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# fix warnings in mysql.  might want to back up mysql data first
-docker-compose build
-docker start sneezydb
-docker exec sneezydb /bin/sh -c "mysql_upgrade"
-docker-compose down
-
